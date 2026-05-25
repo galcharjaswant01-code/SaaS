2ce5bfabf3556544a9634b90ee404be6133f193e
Galchar Jaswant <galcharjaswant01@gmail.com>
2026-05-25 11:56:02 +0530

    prepare for a railway

--- cfehome/views.py
+++ cfehome/views.py
@@ -31,19 +31,20 @@ def my_home_page_view(request, *args, **kwargs):
     my_context = {
         'page_title' : my_title
     }
-    html_ = """
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>{page_title}he part of mind</h1>
-</body>
-</html>
-""".format(**my_context)# page_title = my_title
+    html_ = 'home.html'
+# """
+#     <!DOCTYPE html>
+# <html lang="en">
+# <head> 
+#     <meta charset="UTF-8">
+#     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+#     <title>Document</title>
+# </head>
+# <body>
+#     <h1>{page_title}he part of mind</h1>
+# </body>
+# </html>
+# """.format(**my_context)# page_title = my_title
     
     # html_file_path = this_dir / 'home.html'
     # html_ = html_file_path.read_text()
